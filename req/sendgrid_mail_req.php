27cfc55e8449f4f2811c52e87ef91e08bd950293
David Dunkley <daviddunkley@gmail.com>
2015-08-06 11:43:55 +0100

    Change to use SendGrid Mail Client

--- req/sendgrid_mail_req.php
+++ req/sendgrid_mail_req.php
@@ -1,36 +1,28 @@
 <?php 
-require 'third_parties/php_mailer/PHPMailerAutoload.php';
+require("sendgrid-php.php");
 
 function sendgrid_mail($from_addr, $to_addr, $subject, $html_body) {
 
-	$mail = new PHPMailer;
-
-	//$mail->SMTPDebug = 3;                          	// Enable verbose debug output
-
-	$mail->isSMTP();                                    // Set mailer to use SMTP 
-	$mail->Host = 'smtp.sendgrid.net';             		// Specify main/backup SMTP servers 
-	$mail->SMTPAuth = true;                             // Enable SMTP authentication 
-	$mail->Username = getenv('SENDGRID_USERNAME');    	// SMTP username 
-	$mail->Password = getenv('SENDGRID_PASSWORD');    	// SMTP password 
-	$mail->SMTPSecure = 'tls';                          // Enable TLS/SSL encryption 
-	$mail->Port = 587;                                  // TCP port to connect to
-
-	$mail->From = $from_addr; 
-	$mail->addReplyTo($from_addr);
-	$mail->addAddress($to_addr); 	 			   	   // Add a recipient
-
-	$mail->WordWrap = 50;                              // Set word wrap to 50 characters 
-	$mail->isHTML(true);                               // Set email format to HTML
-
-	$mail->Subject = $subject; 
-	$mail->Body    = $html_body;
-
-	if(!$mail->send()) { 
-	    echo 'Message could not be sent.'; 
-	    echo 'Mailer Error: ' . $mail->ErrorInfo; 
+	$sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
+	
+	$email = new SendGrid\Email();
+	$email
+	    ->addTo($to_addr)
+	    ->setFrom($from_addr)
+	    ->setSubject($subject)
+	    ->setHtml($html_body);
+	
+	try {
+	    $sendgrid->send($email);
+	} 
+	catch(\SendGrid\Exception $e) {
+	    echo $e->getCode() . "\n";
+	    foreach($e->getErrors() as $er) {
+	        echo $er;
+	    }
 		return false;
-	} else { 
-	    return true; 
 	}
+	
+	return true;
 }
 ?>
